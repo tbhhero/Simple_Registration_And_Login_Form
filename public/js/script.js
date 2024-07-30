00f7fbbb16e986a03b68243050a0bb702cdcabcf
Dhiraj Gupta <129402013+tbhhero@users.noreply.github.com>
2024-07-30 23:29:55 +0530

    script for registrstion

---
+++ public/js/script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('.login-form');
+  
+    form.addEventListener('submit', (event) => {
+      event.preventDefault(); // Prevent the default form submission
+  
+      const name = document.getElementById('name').value;
+      const email = document.getElementById('email').value;
+      const username = document.getElementById('username').value;
+      const password = document.getElementById('password').value;
+  
+      console.log('Form submitted!');
+      console.log('Name:', name);
+      console.log('Email:', email);
+      console.log('Username:', username);
+      console.log('Password:', password);
+  
+      // Add your form submission logic here
+      // For example, send data to a server using fetch or XMLHttpRequest
+    });
+  });
+  
